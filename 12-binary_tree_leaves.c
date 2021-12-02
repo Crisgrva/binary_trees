a74a7b9e091daf708ba9821e4a2b52d63e41ff2e
IbetheRamada <valerdagd@gmail.com>
2021-12-02 13:29:40 -0500

    add comment in 12 task

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - function that measures the size of a binary tree
+ * binary_tree_leaves - Write a function that counts 
+ * the leaves in a binary tree
  * @tree:  is a pointer to the root node of the tree to traverse
  * Return: If tree is NULL, your function must return 0
  */
