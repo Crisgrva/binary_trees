df02c3c712f88ab9901b9cbd950d45b2700b4387
Crisgrva <crstngranada@gmail.com>
2021-12-02 15:35:41 -0500

    add task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,32 +18,21 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_balance - function that counts the nodes
- * with at least 1 child in a binary tree
- * @tree:  is a pointer to the root node of the tree
- * to count the number of nodes
+ * binary_tree_is_full - function that checks if a binary tree is full
+ * @tree:  is a pointer to the root node of the tree to traverse
  * Return: If tree is NULL, your function must return 0
  */
-
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t height_l;
-	size_t height_r;
-
 	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 
-	if (tree->left)
-		height_l = binary_tree_height(tree->left) + 1;
-	else
-		height_l = 0;
-
-	if (tree->right)
-		height_r = binary_tree_height(tree->right) + 1;
-	else
-		height_r = 0;
-
-	return (height_l - height_r);
+	return ((binary_tree_is_full(tree->left) == 1) &&
+					(binary_tree_is_full(tree->right) == 1)
+				? 1
+				: 0);
 }
 
 /**
@@ -57,7 +46,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-
-	return (binary_tree_balance(tree->left) ==
-		binary_tree_balance(tree->right) ? 1 : 0);
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	{
+		return ((binary_tree_is_full(tree->left) == 1) &&
+						(binary_tree_is_full(tree->right) == 1)
+					? 1
+					: 0);
+	}
+	return (0);
 }
