7145caf4feeb4352450e4325f4524f5b9e09b4eb
Crisgrva <crstngranada@gmail.com>
2021-12-02 16:58:12 -0500

    fix task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -58,11 +58,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (1);
+	if (binary_tree_balance(tree) == 0)
+		return ((binary_tree_is_perfect(tree->left) ==
+			binary_tree_is_perfect(tree->right)) ? 1 : 0);
 
-	return (binary_tree_balance(tree->left) ==
-					binary_tree_balance(tree->right)
-				? 1
-				: 0);
+	return (0);
 }
